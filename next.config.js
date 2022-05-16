59a1dc0abac4a777c37c9b4f86446aba9b63759b
“juwanpetty” <jchpetty@gmail.com>
2022-05-16 09:52:05 -0400

    Set up sanity studio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
+
+module.exports = {
+  nextConfig,
+  rewrites: () => [STUDIO_REWRITE],
+};
